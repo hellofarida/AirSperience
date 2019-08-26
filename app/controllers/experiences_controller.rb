cc6f1fd77904f6f9ec913e11b351ec3124a7c5a0
Abhishek Samariya <asamariya@gmail.com>
2019-08-26 17:46:22 +1000

    Created add experience listing

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,2 +1,22 @@
 class ExperiencesController < ApplicationController
+  def new
+    @experience = Experience.new
+  end
+
+  def create
+    @experience = Experience.new(experience_params)
+    @experience.owner = current_user
+    if @experience.save
+      redirect_to root_path
+    else
+
+      render :new
+    end
+  end
+
+  private
+
+  def experience_params
+    params.require(:experience).permit(:title, :description, :price, :photo)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_26_041809) do
+ActiveRecord::Schema.define(version: 2019_08_26_070212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_08_26_041809) do
     t.bigint "owner_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.index ["owner_id"], name: "index_experiences_on_owner_id"
   end
 
