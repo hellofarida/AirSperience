ae3df2876447056030d86b46b4d8e10299d8bd37
Lelde Eke <lelde.eke@gmail.com>
2019-08-27 16:16:24 +1000

    temporary card design for dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :experiences, only: [:index, :show, :new, :create, :destroy]
+  resources :experiences, only: [:index, :show, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "dashboard", to: "users#dashboard"
+  # get "restaurants/:id", to: "restaurants#show"
 end
